c638abb601ce8b579fb25b8d32af0b82663474af
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-01-10 17:31:59 +0100

    Changed CallStack interface

--- src/jsonrpc.CallStack.js
+++ src/jsonrpc.CallStack.js
@@ -1,26 +1,23 @@
 jsonrpc = window.jsonrpc || { };
 
-jsonrpc.CallStack = function () {
+jsonrpc.CallStack = function (enterFn, exitFn, scope) {
 	this._counter = 0;
+	this._enterFn = enterFn;
+	this._exitFn = exitFn;
+	this._scope = scope || this;
 };
 
 jsonrpc.CallStack.prototype = {
-	wrapEnterFn: function (fn, scope) {
-		var me = this;
-		return function () {
-			me._counter = (me._counter < 0 ? 1 : me._counter + 1);
-			if(me._counter === 1) {
-				fn.apply(this, arguments);
-			}
-		};
+	enter: function () {
+		this._counter = (this._counter < 0 ? 1 : this._counter + 1);
+		if(this._counter === 1) {
+			this._enterFn.apply(this._scope, arguments);
+		}
 	},
-	wrapExitFn: function (fn) {
-		var me = this;
-		return function () {
-			me._counter -= 1;
-			if (me._counter === 0) {
-				fn.apply(this, arguments);
-			}
-		};
+	exit: function (fn) {
+		this._counter -= 1;
+		if (this._counter === 0) {
+			this._exitFn.apply(this._scope, arguments);
+		}
 	}
 };
--- test/jsonrpc.CallStackTest.js
+++ test/jsonrpc.CallStackTest.js
@@ -1,42 +1,44 @@
 describe('jsonrpc.CallStack', function () {
-	var target;
+	var target, enterFn, exitFn, scope;
 
 	beforeEach(function () {
-		target = new jsonrpc.CallStack();
+		enterFn = jasmine.createSpy();
+		exitFn = jasmine.createSpy();
+		scope = {};
+		target = new jsonrpc.CallStack(enterFn, exitFn, scope);
 	});
 
 	it('should keep scope and parameters', function () {
 		var scope = {},		
 			enterFn = jasmine.createSpy(),
-			wrappedEnterFn = target.wrapEnterFn(enterFn),
 			exitFn = jasmine.createSpy(),
-			wrappedExitFn = target.wrapExitFn(exitFn);
+			target = new jsonrpc.CallStack(enterFn, exitFn, scope);
+			
+		target.enter(1, 2, 3);
+		target.exit(4, 5, 6);
 		
-		wrappedEnterFn.apply(scope, [ 'args' ]);
-		
-		wrappedExitFn.apply(scope, [ 'args' ]);
-		
-		expect(enterFn).toHaveBeenCalledWith('args');
+		expect(enterFn).toHaveBeenCalledWith(1, 2, 3);
 		expect(enterFn.mostRecentCall.object).toBe(scope);
 
-		expect(exitFn).toHaveBeenCalledWith('args');
+		expect(exitFn).toHaveBeenCalledWith(4, 5, 6);
 		expect(exitFn.mostRecentCall.object).toBe(scope);
 	});
 	
 	it('should simmetrically invoke enter and exit fn', function () {
 		var count = 0,
 			enter = [],
-			exit = [],
-			enterFn = target.wrapEnterFn(function () { enter.push(++count); }),
-			exitFn = target.wrapExitFn(function () { exit.push(++count); });
+			exit = [];
+		var target, scope;
+		scope = {};
+		target = new jsonrpc.CallStack(function () { enter.push(++count); }, function () { exit.push(++count); }, scope);
 		
-		exitFn();
-		enterFn();
-		enterFn();
-		exitFn();
-		exitFn();
-		exitFn();
-		enterFn();
+		target.exit();
+		target.enter();
+		target.enter();
+		target.exit();
+		target.exit();
+		target.exit();
+		target.enter();
 		
 		expect(enter).toEqual([1,3]);
 		expect(exit).toEqual([2]);
