bde6b3bab9f55c0838c2acde087b6d740ef9841a
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-01-10 16:25:38 +0100

    Observable now represent a single event

--- test/jsonrpc.ObservableTest.js
+++ test/jsonrpc.ObservableTest.js
@@ -9,19 +9,16 @@ describe('jsonrpc.Observable', function () {
 		var scope = {},
 			ev1h1 = jasmine.createSpy(),
 			ev1s1 = {},
-			ev1h2 = jasmine.createSpy(),
-			ev2h1 = jasmine.createSpy();
+			ev1h2 = jasmine.createSpy();
 		
-		target.bind('ev1', ev1h1, ev1s1);
-		target.unbind(target.bind('ev1', ev1h2));
-		target.bind('ev2', ev2h1);
+		target.bind(ev1h1, ev1s1);
+		target.unbind(target.bind(ev1h2));
 		
-		target.trigger('ev1', 1, 2, 3);
+		target.trigger(1, 2, 3);
 		
 		expect(ev1h1).toHaveBeenCalledWith(1, 2, 3);
 		expect(ev1h1.callCount).toEqual(1);
 		expect(ev1h1.mostRecentCall.object).toEqual(ev1s1);
 		expect(ev1h2).not.toHaveBeenCalled();
-		expect(ev2h1).not.toHaveBeenCalled();
 	});
 });
